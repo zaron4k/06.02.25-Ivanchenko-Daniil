bde194fa7698a8c34c21c325c55fea6f3763cd04
zaron4k <76218026+zaron4k@users.noreply.github.com>
2025-02-05 22:30:35 +0200

    2
    2

---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Time time1 = new Time();
+            time1.SetTime(2, 30, 45);
+            Console.WriteLine(time1.GetTimeInfo());
+            Console.WriteLine("Общее количество секунд: " + time1.TotalSeconds());
+            Time time2 = new Time(3, 15);
+            Console.WriteLine(time2.GetTimeInfo());
+            Console.WriteLine("Общее количество минут: " + time2.TotalMinutes());
+            Console.Write("Введите часы: ");
+            int h = int.Parse(Console.ReadLine());
+            Console.Write("Введите минуты: ");
+            int m = int.Parse(Console.ReadLine());
+            Console.Write("Введите секунды: ");
+            int s = int.Parse(Console.ReadLine());
+            Time time3 = new Time(h, m, s);
+            Console.WriteLine(time3.GetTimeInfo());
+            Console.WriteLine("Общее количество секунд: " + time3.TotalSeconds());
+        }
+    }
+}
---
+++ ConsoleApp10/Time.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    internal class Time
+    {
+        private int hours;
+        private int minutes;
+        private int seconds;
+        public Time()
+        {
+            hours = 0;
+            minutes = 0;
+            seconds = 0;
+        }
+        public Time(int h)
+        {
+            hours = h;
+            minutes = 0;
+            seconds = 0;
+        }
+        public Time(int h, int m)
+        {
+            hours = h;
+            minutes = m;
+            seconds = 0;
+        }
+        public Time(int h, int m, int s)
+        {
+            hours = h;
+            minutes = m;
+            seconds = s;
+        }
+        public int TotalMinutes()
+        {
+            return hours * 60 + minutes;
+        }
+        public int TotalSeconds()
+        {
+            return (hours * 3600) + (minutes * 60) + seconds;
+        }
+        public void SetTime(int h, int m, int s)
+        {
+            hours = h;
+            minutes = m;
+            seconds = s;
+        }
+        public string GetTimeInfo()
+        {
+            return $"Время: {hours:D2}:{minutes:D2}:{seconds:D2}";
+        }
+    }
+}
