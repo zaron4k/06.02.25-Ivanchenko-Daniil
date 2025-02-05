53e3090e8dc3ea559419466c7771135c39d4f5c7
zaron4k <76218026+zaron4k@users.noreply.github.com>
2025-02-05 22:22:15 +0200

    1
    1

---
+++ ConsoleApp9/Calculate.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    internal class Calculate
+    {
+
+        public double Num1;
+        public double Num2;
+        public Calculate()
+        {
+            Num1 = 0.0;
+            Num2 = 0.0;
+        }
+        public Calculate(double num1)
+        {
+            Num1 = num1;
+            Num2 = 0.0;
+        }
+        public Calculate(double num1, double num2)
+        {
+            Num1 = num1;
+            Num2 = num2;
+        }
+        public override string ToString()
+        {
+            return $"Calculate(Num1={Num1}, Num2={Num2})";
+        }
+        public double ComputeSqrtProduct()
+        {
+            return Math.Sqrt(Num1 * Num2);
+        }
+    }
+}
---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculate obj1 = new Calculate();
+            obj1.Num1 = 4.0;
+            obj1.Num2 = 9.0;
+            Console.WriteLine(obj1);
+            Console.WriteLine("Корень из произведения чисел: " + obj1.ComputeSqrtProduct());
+            Calculate obj2 = new Calculate(3.0, 16.0);
+            Console.WriteLine(obj2);
+            Console.WriteLine("Корень из произведения чисел: " + obj2.ComputeSqrtProduct());
+            Console.Write("Введите первое число: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите второе число: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+            Calculate obj3 = new Calculate(num1, num2);
+            Console.WriteLine(obj3);
+            Console.WriteLine("Корень из произведения чисел: " + obj3.ComputeSqrtProduct());
+        }
+    }
+}
